3dad738ab210bc17754f3b6a7b09c0edcf35ad26
Tenusia <110511640+Tenusia@users.noreply.github.com>
2023-09-20 23:18:41 +0200

    Add scoring system

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var lives = 3
+var score = 0
 
 @onready var player = $Player
 
@@ -15,3 +16,11 @@ func _on_player_took_damage():
 	else:
 		print(lives)
 		
+
+func _on_enemy_spawner_enemy_spawned(enemy_instance):
+	enemy_instance.connect("died", _on_enemy_died)
+	add_child(enemy_instance)
+	
+func _on_enemy_died():
+	score += 100
+	print("Score: " + str(score))
