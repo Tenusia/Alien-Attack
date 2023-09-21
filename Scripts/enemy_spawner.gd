4cc38d7ecc20655cbba495b429d22c30052cd023
Tenusia <110511640+Tenusia@users.noreply.github.com>
2023-09-21 21:51:19 +0200

    Add path follow enemy

--- Scripts/enemy_spawner.gd
+++ Scripts/enemy_spawner.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
 signal enemy_spawned(enemy_instance)
+signal path_enemy_spawned(path_enemy_instance)
+
 var enemy_scene = preload("res://Scenes/enemy.tscn")
+var path_enemy_scene = preload("res://Scenes/path_enemy.tscn")
 
 @onready var spawn_positions = $SpawnPositions
 
@@ -15,3 +18,10 @@ func spawn_enemy():
 	var enemy_instance = enemy_scene.instantiate()
 	enemy_instance.global_position = random_spawn_position.global_position
 	emit_signal("enemy_spawned", enemy_instance)
+
+func _on_path_timer_timeout():
+	spawn_path_enemy()
+
+func spawn_path_enemy():
+	var path_enemy_instance = path_enemy_scene.instantiate()
+	emit_signal("path_enemy_spawned", path_enemy_instance)
