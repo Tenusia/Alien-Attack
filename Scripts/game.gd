3eabd480e0628fd14f5b8128b39b83e0e1f517b8
Tenusia <110511640+Tenusia@users.noreply.github.com>
2023-09-21 17:34:15 +0200

    Add lives UI

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -8,18 +8,17 @@ var score = 0
 
 func _ready():
 	hud.set_score_label(score)
+	hud.set_lives(lives)
 
 func _on_deathzone_area_entered(area):
 	area.die()
 
 func _on_player_took_damage():
 	lives -= 1
+	hud.set_lives(lives)
 	if lives == 0:
 		print("GAME OVER")
 		player.die()
-	else:
-		print(lives)
-		
 
 func _on_enemy_spawner_enemy_spawned(enemy_instance):
 	enemy_instance.connect("died", _on_enemy_died)
